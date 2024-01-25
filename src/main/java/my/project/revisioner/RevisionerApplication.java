f52871288b42ccc7c90a2445f264dd1b50255dd8
Melekhin <s.melekhin@bftcom.com>
2024-01-25 16:52:26 +0300

    init

---
+++ src/main/java/my/project/revisioner/RevisionerApplication.java
@@ -0,0 +1,13 @@
+package my.project.revisioner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevisionerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/project/revisioner/RevisionerApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.revisioner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
